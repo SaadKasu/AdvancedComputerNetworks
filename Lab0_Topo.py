50cefd518c1bd8bc274e82c7c64e0e541385ceb7
SaadKasu <saadkasu@protonmail.com>
2025-04-18 21:52:34 +0200

    Update_4_With_Self

--- Lab0_Topo.py
+++ Lab0_Topo.py
@@ -9,17 +9,17 @@ class BridgeTopo(Topo):
 
 #net = Mininet(link=TCLink, switch=OVSBridge)
 
-h1 = net.addHost(h1)
-h2 = net.addHost(h2)
-h3 = net.addHost(h3)
-h4 = net.addHost(h4)
+h1 = self.addHost(h1)
+h2 = self.addHost(h2)
+h3 = self.addHost(h3)
+h4 = self.addHost(h4)
 
-s1 = net.addSwitch(s1)
-s2 = net.addSwitch(s2)
+s1 = self.addSwitch(s1)
+s2 = self.addSwitch(s2)
 
-l1 = addLink(s1,s2, bw=20, delay='45ms')
-l2 = addLink(h1,s1, bw=15, delay='10ms')
-l3 = addLink(h2,s1, bw=15, delay='10ms')
-l4 = addLink(h3,s2, bw=15, delay='10ms')
-l5 = addLink(h4,s2, bw=15, delay='10ms')
+l1 = self.addLink(s1,s2, bw=20, delay='45ms')
+l2 = self.addLink(h1,s1, bw=15, delay='10ms')
+l3 = self.addLink(h2,s1, bw=15, delay='10ms')
+l4 = self.addLink(h3,s2, bw=15, delay='10ms')
+l5 = self.addLink(h4,s2, bw=15, delay='10ms')
 
