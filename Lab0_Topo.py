ada401344a14cb3ef623423d69d63a462e571ee1
SaadKasu <saadkasu@protonmail.com>
2025-04-18 22:10:07 +0200

    Update_8_With_OVSBridges

--- Lab0_Topo.py
+++ Lab0_Topo.py
@@ -14,13 +14,13 @@ class BridgeTopo(Topo):
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
-        s1 = self.addSwitch('s1', cls = OVSBridge)
-        s2 = self.addSwitch('s1', cls = OVSBridge)
+        s1 = self.addSwitch('s1', cls = 'OVSBridge')
+        s2 = self.addSwitch('s1', cls = 'OVSBridge')
 
-        l1 = self.addLink(s1,s2, bw=20, delay='45ms', cls = TCLink)
-        l2 = self.addLink(h1,s1, bw=15, delay='10ms', cls = TCLink)
-        l3 = self.addLink(h2,s1, bw=15, delay='10ms', cls = TCLink)
-        l4 = self.addLink(h3,s2, bw=15, delay='10ms', cls = TCLink)
-        l5 = self.addLink(h4,s2, bw=15, delay='10ms', cls = TCLink)
+        l1 = self.addLink(s1,s2, bw=20, delay='45ms', cls = 'TCLink')
+        l2 = self.addLink(h1,s1, bw=15, delay='10ms', cls = 'TCLink')
+        l3 = self.addLink(h2,s1, bw=15, delay='10ms', cls = 'TCLink')
+        l4 = self.addLink(h3,s2, bw=15, delay='10ms', cls = 'TCLink')
+        l5 = self.addLink(h4,s2, bw=15, delay='10ms', cls = 'TCLink')
 
 topos = {'bridge': (lambda: BridgeTopo())}
