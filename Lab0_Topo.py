90e0d5e30cddf63d67d12b8ad8ed2973e3c60052
SaadKasu <saadkasu@protonmail.com>
2025-04-18 21:53:43 +0200

    Update_5_With_Self_And_Indentation

--- Lab0_Topo.py
+++ Lab0_Topo.py
@@ -3,23 +3,23 @@
 from mininet.topo import Topo
 
 class BridgeTopo(Topo):
-     def __init__(self):
+    def __init__(self):
 
-       Topo.__init__(self)
+        Topo.__init__(self)
 
 #net = Mininet(link=TCLink, switch=OVSBridge)
 
-h1 = self.addHost(h1)
-h2 = self.addHost(h2)
-h3 = self.addHost(h3)
-h4 = self.addHost(h4)
+        h1 = self.addHost(h1)
+        h2 = self.addHost(h2)
+        h3 = self.addHost(h3)
+        h4 = self.addHost(h4)
 
-s1 = self.addSwitch(s1)
-s2 = self.addSwitch(s2)
+        s1 = self.addSwitch(s1)
+        s2 = self.addSwitch(s2)
 
-l1 = self.addLink(s1,s2, bw=20, delay='45ms')
-l2 = self.addLink(h1,s1, bw=15, delay='10ms')
-l3 = self.addLink(h2,s1, bw=15, delay='10ms')
-l4 = self.addLink(h3,s2, bw=15, delay='10ms')
-l5 = self.addLink(h4,s2, bw=15, delay='10ms')
+        l1 = self.addLink(s1,s2, bw=20, delay='45ms')
+        l2 = self.addLink(h1,s1, bw=15, delay='10ms')
+        l3 = self.addLink(h2,s1, bw=15, delay='10ms')
+        l4 = self.addLink(h3,s2, bw=15, delay='10ms')
+        l5 = self.addLink(h4,s2, bw=15, delay='10ms')
 
