5c15e20693b84afcd7cd99aa140356123d29c251
SaadKasu <saadkasu@protonmail.com>
2025-04-19 00:46:46 +0200

    Update_9_With_New_Import

--- Lab0_Topo.py
+++ Lab0_Topo.py
@@ -1,6 +1,7 @@
 # Python Script For Lab 0
 #!/usr/bin/python
 from mininet.topo import Topo
+from mininet.node import OVSBridge, TCLink
 
 class BridgeTopo(Topo):
     def __init__(self):
@@ -14,13 +15,15 @@ class BridgeTopo(Topo):
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
-        s1 = self.addSwitch('s1', cls = 'OVSBridge')
-        s2 = self.addSwitch('s1', cls = 'OVSBridge')
+        s1 = self.addSwitch('s1', cls = OVSBridge)
+        s2 = self.addSwitch('s1', cls = OVSBridge)
 
-        l1 = self.addLink(s1,s2, bw=20, delay='45ms', cls = 'TCLink')
-        l2 = self.addLink(h1,s1, bw=15, delay='10ms', cls = 'TCLink')
-        l3 = self.addLink(h2,s1, bw=15, delay='10ms', cls = 'TCLink')
-        l4 = self.addLink(h3,s2, bw=15, delay='10ms', cls = 'TCLink')
-        l5 = self.addLink(h4,s2, bw=15, delay='10ms', cls = 'TCLink')
+        l1 = self.addLink(s1,s2, bw=20, delay='45ms', cls = TCLink)
+        l2 = self.addLink(h1,s1, bw=15, delay='10ms', cls = TCLink)
+        l3 = self.addLink(h2,s1, bw=15, delay='10ms', cls = TCLink)
+        l4 = self.addLink(h3,s2, bw=15, delay='10ms', cls = TCLink)
+        l5 = self.addLink(h4,s2, bw=15, delay='10ms', cls = TCLink)
+
+
 
 topos = {'bridge': (lambda: BridgeTopo())}
