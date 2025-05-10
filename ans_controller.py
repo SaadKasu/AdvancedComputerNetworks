d554441ac8abfed8e5d9b33f5ef62619b95bccb7
SaadKasu <saadkasu@protonmail.com>
2025-05-10 22:20:27 +0200

    Loggin PKT info removed

--- ans_controller.py
+++ ans_controller.py
@@ -85,8 +85,6 @@ class LearningSwitch(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-    
-        self.logger.info("Ether Type - %s",eth)
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
@@ -105,7 +103,7 @@ class LearningSwitch(app_manager.RyuApp):
         else:
             out_port = ofproto.OFPP_FLOOD
 
-        self.logger.info("packet in %s %s %s %s %s %s", dpid, src, dst, in_port, out_port, pkt)
+        self.logger.info("packet in %s %s %s %s %s", dpid, src, dst, in_port, out_port)
 
         actions = [parser.OFPActionOutput(out_port)]
 
