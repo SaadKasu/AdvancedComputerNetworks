b43ff0b596c58650da396caadf9d7e9fe56ff7ad
SaadKasu <saadkasu@protonmail.com>
2025-05-11 00:39:46 +0200

    Fixed Network

--- run_network.py
+++ run_network.py
@@ -36,10 +36,10 @@ class NetworkTopo(Topo):
         Topo.__init__(self)
 
         # Build the specified network topology here
-        h1 = self.addHost('h1', ip = '10.0.1.2/24')
-        h2 = self.addHost('h2', ip = '10.0.1.3/24')
-        ser = self.addHost('ser', ip = '10.0.2.2/24')
-        ext = self.addHost('ext', ip = '192.168.1.123/24')
+        h1 = self.addHost('h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+        h2 = self.addHost('h2', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
+        ser = self.addHost('ser', ip = '10.0.2.2/24', defaultRoute='via 10.0.2.1')
+        ext = self.addHost('ext', ip = '192.168.1.123/24', defaultRoute='via 192.168.1.1')
 
         s1 = self.addSwitch('s1', cls = OVSKernelSwitch)
         s2 = self.addSwitch('s2', cls = OVSKernelSwitch)
