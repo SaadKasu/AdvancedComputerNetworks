adab94c7749fb34103fbd7bb88d8eaddc9da88f1
SaadKasu <saadkasu@protonmail.com>
2025-05-11 00:20:34 +0200

    Router Implementation

--- ans_controller.py
+++ ans_controller.py
@@ -24,12 +24,10 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet, arp, ipv4
 from ryu.lib.packet import ether_types
 from ryu.lib import mac
-from ryu.topology import event
 
 
 class LearningSwitch(app_manager.RyuApp):
@@ -40,18 +38,11 @@ class LearningSwitch(app_manager.RyuApp):
 
         # Here you can initialize the data structures you want to keep at the controller
         self.mac_to_port = {}
-        self.port_to_own_mac = {
-            1: "00:00:00:00:01:01",
-            2: "00:00:00:00:01:02",
-            3: "00:00:00:00:01:03"
+        self.gateways = {
+            '10.0.1.1': '00:00:00:00:01:01',  # Gateway for subnet 10.0.1.0/24
+            '10.0.2.1': '00:00:00:00:01:02',  # Gateway for subnet 10.0.2.0/24
+            '192.168.1.1': '00:00:00:00:01:03'  # Gateway for Internet/other subnet
         }
-        self.port_to_own_ip = {
-            1: "10.0.1.1",
-            2: "10.0.2.1",
-            3: "192.168.1.1"
-        }
-        self.arp_table = {} 
-
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         
@@ -108,6 +99,35 @@ class LearningSwitch(app_manager.RyuApp):
 
         self.logger.info("packet in %s %s %s %s %s", dpid, src, dst, in_port, out_port)
         
+        # Handle ARP packets (to populate the ARP table)
+        arp_pkt = pkt.get_protocol(arp.arp)
+        if arp_pkt:
+            self.logger.info(f"Received ARP packet: {arp_pkt.src_ip} -> {arp_pkt.dst_ip}")
+            self.arp_table[arp_pkt.src_ip] = arp_pkt.src_mac
+
+            if arp_pkt.opcode == arp.ARP_REQUEST:
+                # If this is an ARP request, reply with ARP_REPLY if we are the target
+                if arp_pkt.dst_ip in self.gateways:
+                    self._send_arp_reply(datapath, pkt, arp_pkt, in_port)
+                return
+
+        # Handle IPv4 packet forwarding (routing)
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ip_pkt:
+            dst_ip = ip_pkt.dst
+            if dst_ip in self.gateways:
+                # If the destination is a local gateway, use the MAC address from the gateway table
+                dst_mac = self.gateways[dst_ip]
+                out_port = self.mac_to_port[dpid].get(dst_mac, ofproto.OFPP_FLOOD)
+            else:
+                # Use routing table for routing between different subnets
+                next_hop_ip = self.routing_table.get(dst_ip)
+                if next_hop_ip and next_hop_ip in self.arp_table:
+                    dst_mac = self.arp_table[next_hop_ip]
+                    out_port = self.mac_to_port[dpid].get(dst_mac, ofproto.OFPP_FLOOD)
+                else:
+                    out_port = ofproto.OFPP_FLOOD
+        
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
@@ -126,5 +146,31 @@ class LearningSwitch(app_manager.RyuApp):
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def _send_arp_reply(self, datapath, pkt, arp_req, port):
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        src_mac = self.gateways.get(arp_req.dst_ip)
+        if not src_mac:
+            return
+
+        arp_reply = packet.Packet()
+        arp_reply.add_protocol(ethernet.ethernet(
+            ethertype=ether_types.ETH_TYPE_ARP,
+            dst=arp_req.src_mac,
+            src=src_mac))
+        arp_reply.add_protocol(arp.arp(
+            opcode=arp.ARP_REPLY,
+            src_mac=src_mac,
+            src_ip=arp_req.dst_ip,
+            dst_mac=arp_req.src_mac,
+            dst_ip=arp_req.src_ip))
+
+        arp_reply.serialize()
+        actions = [datapath.ofproto_parser.OFPActionOutput(port)]
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=datapath.ofproto.OFP_NO_BUFFER,
+            in_port=datapath.ofproto.OFPP_CONTROLLER,
+            actions=actions, data=arp_reply.data)
         datapath.send_msg(out)
 
