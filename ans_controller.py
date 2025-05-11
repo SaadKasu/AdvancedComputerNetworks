071ceeeb00ebd5735f57ef04d3c97c5b61003da0
SaadKasu <saadkasu@protonmail.com>
2025-05-11 18:48:02 +0200

    Syntax error fix 2

--- ans_controller.py
+++ ans_controller.py
@@ -99,7 +99,7 @@ class LearningSwitch(app_manager.RyuApp):
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
         
-        if dspid=3:
+        if dspid==3:
             # Handle ARP packets
             if eth.ethertype == ether_types.ETH_TYPE_ARP and arp_pkt:
                 self.handle_arp(datapath, pkt, arp_pkt, in_port)
