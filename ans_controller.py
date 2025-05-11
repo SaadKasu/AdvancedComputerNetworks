27c4e3723523b457909b4465c3cf8a40ec434b47
SaadKasu <saadkasu@protonmail.com>
2025-05-11 21:13:20 +0200

    Changed Handel IP and ARP fix 1

--- ans_controller.py
+++ ans_controller.py
@@ -119,7 +119,7 @@ class LearningSwitch(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            self.add_flow(datapath, ofproto.OFP_DEFAULT_PRIORITY, datapath.ofproto_parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src), actions, msg.buffer_id)
+            self.add_flow(datapath, ofproto.OFP_DEFAULT_PRIORITY, datapath.ofproto_parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src), actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
