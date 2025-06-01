a9258601f72d431e2ab24040281bed30351c2032
SaadKasu <saadkasu@protonmail.com>
2025-06-01 20:24:29 +0200

    Something is working changes 3

--- sp_routing.py
+++ sp_routing.py
@@ -77,7 +77,7 @@ class SPRouter(app_manager.RyuApp):
 
         while len(Q) > 0:
             u = self.minimum_distance(distance, Q)
-            print("\nU - ", u)
+
             if u in Q : 
                 Q.remove(u)
 
@@ -232,12 +232,10 @@ class SPRouter(app_manager.RyuApp):
 
             if p not in self.found_paths:
                 self.found_paths.append(p)
-                self.install_path(p, ev, src_mac, dst_mac)
+            self.install_path(p, ev, src_mac, dst_mac)
                 # this will be the output port for this switch to redirect the packets to the desired destination
-                out_port = p[0][2]
-                return
-            else:
-                out_port = p[0][2]
+            out_port = p[0][2]
+            return
 
         else:
             # when the dst isn't found then it shall be flooded to all output ports
