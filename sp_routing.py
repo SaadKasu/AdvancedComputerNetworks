c85133082330b7dbc0f1f326cc5f253fd82578e5
SaadKasu <saadkasu@protonmail.com>
2025-06-01 10:33:14 +0200

    SP_Routing_Changed_Logging_18

--- sp_routing.py
+++ sp_routing.py
@@ -284,7 +284,7 @@ class SPRouter(app_manager.RyuApp):
                 return
             print("\n Data path - ",datapath,"\n Datapath id -", datapath.id, "\n Map len-", len(self.switch_mac_table), "\n Map - ", self.switch_mac_table)
             #self.logger.info("\n Map with Key - %s",self.switch_mac_table[datapath.id], "\n Keys - %s",self.switch_mac_table[datapath.id].keys())
-            if datapath.id in self.switch_mac_table.keys() :
+            if datapath.id not in self.switch_mac_table.keys() :
                 self.switch_mac_table[datapath.id] = {}
             if src not in self.switch_mac_table[datapath.id].keys():
                 self.switch_mac_table[datapath.id][src] = (in_port, arp_tpa)  # Updating mac table of this 
