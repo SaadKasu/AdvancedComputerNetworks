e61ba57aef8cef2b69f6e2c48e660fdd07eb6e6b
SaadKasu <saadkasu@protonmail.com>
2025-06-05 09:59:33 +0200

    Self Implementation

--- sp_routing.py
+++ sp_routing.py
@@ -82,6 +82,7 @@ class SPRouter(app_manager.RyuApp):
 
     def dijkstra(self, source, switches):
         dist = {}
+        prev = {}
         for switch in switches :
             dist[switch.dp.id] = float('inf')
             prev[switch.dp.id] = (None,None)
