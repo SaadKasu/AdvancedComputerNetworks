76007183da05eeb2361db5566c465d72a82a5ffb
SaadKasu <saadkasu@protonmail.com>
2025-06-06 21:39:04 +0200

    6th Juni

--- sp_routing.py
+++ sp_routing.py
@@ -66,8 +66,6 @@ class SPRouter(app_manager.RyuApp):
         for switch in self.switches :
             self.dpid_neighbours.setdefault(switch.dp.id, {})
             self.switch_datapath[switch.dp.id] = switch.dp
-            if switch.dp.id == int("00:00:00:00:00:10"):
-                print("\nHELLO")
 
         for link in self.links:
             src = link.src
