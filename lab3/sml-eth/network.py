2fc1cb625fdc5c0f8a3d873002555074236d6d87
SaadKasu <saadkasu@protonmail.com>
2025-07-15 15:43:32 +0200

    lab_3_commit

--- lab3/sml-eth/network.py
+++ lab3/sml-eth/network.py
@@ -1,53 +1,28 @@
-"""
- Copyright (c) 2025 Computer Networks Group @ UPB
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of
- this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to
- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- the Software, and to permit persons to whom the Software is furnished to do so,
- subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- """
 
 from lib import config # do not import anything before this
 from p4app import P4Mininet
 from mininet.topo import Topo
 from mininet.cli import CLI
 import os
-
+ 
 NUM_WORKERS = 2 # TODO: Make sure your program can handle larger values
-
-# Simple logic to allocate IP and MAC addresses based on the worker ID
-def getWorkerIP(wid):
-    return "10.0.0.%d" % (wid + 1)
-
-def getWorkerMAC(wid):
-    return "00:00:00:00:01:%02x" % (wid + 1)
-
+ 
 class SMLTopo(Topo):
     def __init__(self, **opts):
         Topo.__init__(self, **opts)
         # TODO: Implement me. Feel free to modify the constructor signature
         # NOTE: Make sure worker names are consistent with RunWorkers() below
-    # Create the switch
         sw = self.addSwitch('s1')
-
-        # Create the workers
         for i in range(NUM_WORKERS):
+            host_name = 'w%d' % i
             worker = self.addHost(
-                'w%d' % i, ip=getWorkerIP(i), mac=getWorkerMAC(i))
-            self.addLink(worker, sw, port2=i)
-
+                host_name,
+                ip="10.0.0.%d/24" % (i + 1),
+                mac="00:00:00:00:01:%02x" % (i + 1)
+            )
+            self.addLink(worker, sw, port2=i) # Switch port index matches worker rank
+ 
+ 
 def RunWorkers(net):
     """
     Starts the workers and waits for their completion.
@@ -61,21 +36,23 @@ def RunWorkers(net):
         net.get(worker(i)).sendCmd('python worker.py %d > %s' % (i, log_file(i)))
     for i in range(NUM_WORKERS):
         net.get(worker(i)).waitOutput()
-    print("All workers have completed their execution.")  
-
+    print("All workers have completed their execution.")    
+ 
 def RunControlPlane(net):
     """
     One-time control plane configuration
     """
     # TODO: Implement me (if needed)
-    # use functions already defined.
-    sw = net.get('s1')
-    
-    # Use function addMulticastGroup from p4app/src/p4_mininet.py to add a multicast group
+    print("Configuring control plane...")
+    s1 = net.get('s1')
+ 
+    # configure the multicast group
+    # ALL_WORKERS_MCAST_GROUP = 1 corresponding to the P4 program
     mc_ports = [i for i in range(NUM_WORKERS)]
-    sw.addMulticastGroup(mgid=1, ports=mc_ports)
+    s1.addMulticastGroup(mgid=1, ports=mc_ports)
     print("Multicast group 1 configured with ports:", mc_ports)
-
+ 
+ 
 topo = SMLTopo() # TODO: Create an SMLTopo instance
 net = P4Mininet(program="p4/main.p4", topo=topo)
 net.run_control_plane = lambda: RunControlPlane(net)
